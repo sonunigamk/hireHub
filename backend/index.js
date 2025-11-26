a7e18d255acc3cc87f7450e347116a36bd2e80f7
Sonu Kumar <sonunigam0382002@gmail.com>
2025-11-26 15:04:38 +0530

    models and DB connection completed

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,36 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// CORS Configuration
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("working fine");
+  return res.status(200).json({
+    message: "Server is working fine",
+    success: true,
+  });
 });
 
-app.listen(3000, () => console.log("listen properly"));
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
