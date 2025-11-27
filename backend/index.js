3bd41450becc8a67edee2b1c02f957643604ec4d
Sonu Kumar <sonunigam0382002@gmail.com>
2025-11-27 21:30:54 +0530

    application controller and routes done

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 dotenv.config();
 
@@ -38,6 +39,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
